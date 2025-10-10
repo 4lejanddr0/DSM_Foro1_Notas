28e318b418747bc0e68e131c9d8125500ff8c47a
Alejandro Campos <107518565+4lejanddr0@users.noreply.github.com>
2025-10-09 22:39:38 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/dsm_foro1_notas/MainActivity.kt
+++ app/src/main/java/com/example/dsm_foro1_notas/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import com.example.dsm_foro1_notas.ui.AppNav
 import com.example.dsm_foro1_notas.ui.components.FancyBackground
-import com.example.dsm_foro1_notas.ui.theme.DSM_Foro1_NotasTheme // <-- abre ui/theme/Theme.kt y verifica el nombre del tema
+import com.example.dsm_foro1_notas.ui.theme.DSM_Foro1_NotasTheme 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DSM_Foro1_NotasTheme { // usa aquí el nombre exacto del composable de tu Theme.kt
+            DSM_Foro1_NotasTheme { 
                 Box {
                     FancyBackground()
                     AppNav()
